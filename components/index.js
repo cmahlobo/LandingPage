d08dd3ff872b62f77063945ac8fd3b94ad9a0981
cmahlobo <lcmahlobo@gmail.com>
2019-06-23 16:47:40 -0600

    just need to add files from other computer for tabs. either tonight or in the morning

---
+++ components/index.js
@@ -0,0 +1,11 @@
+donors = document.querySelector('.buttons');
+donors.addEventListener('click', function(event){
+    alert('Thanks for your Donation!');
+})
+
+add = document.querySelector('.subs');
+add.addEventListener('click', function(event){
+    alert('Thank you! Your School has been submitted for Review!');
+})
+
+/* Start Tab Code Here*/
