035f6c6de5df3a7879d663772327a814deb50b59
cmahlobo <lcmahlobo@gmail.com>
2019-06-24 16:58:02 -0600

    commit didn't work this morning, just saw and putting it in there

--- components/index.js
+++ components/index.js
@@ -9,3 +9,19 @@ add.addEventListener('click', function(event){
 })
 
 /* Start Tab Code Here*/
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
